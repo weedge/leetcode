be4251be7f0d262de2db8f8fc1d7611e7c1c2678
weedge <weege007@gmail.com>
2021-11-06 23:15:28 +0800

    add RBT/palindrome-partitioning.go

---
+++ RBT/palindrome-partitioning.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func partition(s string) [][]string {
+	n := len(s)
+	if n == 0 {
+		return [][]string{}
+	}
+	ans := [][]string{}
+	set := []string{}
+
+	var dfs func(cur int)
+	dfs = func(cur int) {
+		if cur == n {
+			tmp := make([]string, len(set))
+			copy(tmp, set)
+			ans = append(ans, tmp)
+		}
+		for i := cur; i < n; i++ {
+			if !checkPalindrome(s[cur : i+1]) {
+				continue
+			}
+			set = append(set, s[cur:i+1])
+			dfs(cur + 1)
+			set = set[:len(set)-1]
+		}
+
+	}
+	dfs(0)
+
+	return ans
+}
+
+func checkPalindrome(s string) bool {
+	n := len(s)
+	if n == 0 {
+		return false
+	}
+
+	l, r := 0, n-1
+	for l < r {
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+
+	return true
+}
+
+func main() {
+	testCases := []string{
+		"aab",
+		"abb",
+		"baa",
+		"aa",
+	}
+
+	for _, testCase := range testCases {
+		res := partition(testCase)
+		fmt.Println(res)
+		return
+	}
+}
