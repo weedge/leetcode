62873652c4998ffa73e97743a8e49cd398099aa9
weedge <weege007@gmail.com>
2021-11-06 11:48:09 +0800

    add RBT/letter-case-permutation.go

--- RBT/letter-case-permutation.go
+++ RBT/letter-case-permutation.go
@@ -13,9 +13,10 @@ func letterCasePermutation(s string) []string {
 	var dfs func(cur int)
 	dfs = func(cur int) {
 		if cur == n {
-			tmp := make([]byte, len(set))
-			copy(tmp, set)
-			ans = append(ans, string(tmp))
+			//tmp := make([]byte, len(set))
+			//copy(tmp, set)
+			//ans = append(ans, string(tmp))
+			ans = append(ans, string(set))
 			return
 		}
 		if s[cur] >= '0' && s[cur] <= '9' {
@@ -26,7 +27,7 @@ func letterCasePermutation(s string) []string {
 			dfs(cur + 1)
 			set[cur] = s[cur] - 32
 			dfs(cur + 1)
-		} else if s[cur] >= 'A' && s[cur] <= 'Z' {// 回溯 大写 变成 小写 再次搜索
+		} else if s[cur] >= 'A' && s[cur] <= 'Z' { // 回溯 大写 变成 小写 再次搜索
 			set[cur] = s[cur]
 			dfs(cur + 1)
 			set[cur] = s[cur] + 32
