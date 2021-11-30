751228660b732b31be6b99f248ea6746f7ec0d28
weedge <weege007@gmail.com>
2021-11-30 10:45:07 +0800

    fix

--- DP/merge-Intervals.go
+++ DP/merge-Intervals.go
@@ -5,24 +5,56 @@ import (
 	"sort"
 )
 
+// 直男思维(greed)：从局部解 => 下一步局部解 => 解
+// 合并区间: 左边排序
+// [[2,4],[5,7],[8,11]] [12,13]  => [[2,4],[5,7],[8,11],[12,13]]
+// [[2,4],[5,7],[8,11]] [9,10]  => [[2,4],[5,7],[8,11]]
+// [[2,4],[5,7],[8,11]] [10,12]  => [[2,4],[5,7],[8,12]]
+func mergeIntervalsV2(arr [][2]int) [][2]int {
+	n := len(arr)
+	if n == 0 {
+		return [][2]int{}
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i][0] <= arr[j][0]
+	})
+	//fmt.Println(arr)
+
+	res := [][2]int{arr[0]}
+	for i := 1; i < n; i++ {
+		m := len(res)
+		if res[m-1][1] < arr[i][0] {
+			res = append(res, arr[i])
+		} else {
+			tmp := [2]int{res[m-1][0], res[m-1][1]}
+			if res[m-1][1] < arr[i][1] {
+				tmp = [2]int{res[m-1][0], arr[i][1]}
+			}
+			res[m-1] = tmp
+		}
+	}
+
+	return res
+}
+
 //dp[i]{l,h} = dp[i-1]{min(l,arr[i][0]),max(h,arr[i][1])} (arr[i][0]<h)
 //l = dp[i][0][0]
 //h = dp[i][len(dp[i])-1][1]
 //dp[i]{l,h} = dp[i-1]{l,h},{arr[i][0],arr[i][1]} (arr[i][0]>h)
 // 时间复杂度：O(n*log(n))
-// 合并重复区间:
+// 合并重复区间:(右边排序)
 // [[2,4],[5,7],[8,9]] [6,10] => [[2,4],[5,10]]
 // [[2,4],[5,7],[8,9]] [10,11] => [[2,4],[5,7],[8,9],[10,11]]
 func mergeIntervals(arr [][2]int) [][2]int {
-	sort.Slice(arr, func(i, j int) bool {
-		if arr[i][1] > arr[j][1] {
-			return false
-		}
-		return true
-	})
-
-	//fmt.Println(arr)
 	n := len(arr)
+	if n == 0 {
+		return [][2]int{}
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i][1] <= arr[j][1]
+	})
+	//fmt.Println(arr)
+
 	res := [][2]int{arr[0]}
 	for i := 1; i < n; i++ {
 		m := len(res)
@@ -107,7 +139,8 @@ func main() {
 	}
 
 	for _, testCase := range testCases {
-		res := mergeIntervals(testCase)
+		//res := mergeIntervals(testCase)
+		res := mergeIntervalsV2(testCase)
 		fmt.Println(testCase)
 		fmt.Println(res)
 	}
